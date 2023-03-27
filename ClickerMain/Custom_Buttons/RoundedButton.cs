9bed16d9e8c3bbc3d55259724343f3c3d6bd2e21
Artem Maksymov <armaksimov3@gmail.com>
2023-03-27 19:26:42 -0400

    Added a class allowing to create round(ed) buttons. The class itself and its' functionality is mainly based on the tutorial linked in the comment tab in the class itself.

---
+++ ClickerMain/Custom_Buttons/RoundedButton.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing.Drawing2D;
+
+// This code is adapted from the following video guide to creating custom buttons in WinForms: https://www.youtube.com/watch?v=u8SL5g9QGcI&ab_channel=RJCodeAdvanceEN
+
+namespace ClickerMain.Custom_Buttons
+{
+    public class RoundedButton : Button
+    {
+        private int borderSize = 0;
+        private int borderRadius = 0;
+        private Color borderColor = Color.White;
+
+        public int BorderSize
+        {
+            get 
+            { 
+                return borderSize; 
+            }
+            set
+            {
+                borderSize = value;
+                this.Invalidate();
+            }
+        }
+
+        public int BorderRadius
+        {
+            get 
+            { 
+                return borderRadius; 
+            }
+            set
+            {
+                borderRadius = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color BorderColor
+        {
+            get { 
+                return borderColor; 
+            }
+            set
+            {
+                borderColor = value;
+                this.Invalidate();
+            }
+        }
+
+        public Color BackgroundColor
+        {
+            get
+            {
+                return this.BackColor;
+            }
+            set
+            {
+                this.BackColor = value;
+            }
+        }
+
+        public Color TextColor
+        {
+            get
+            {
+                return this.ForeColor;
+            }
+            set
+            {
+                this.ForeColor = value;
+            }
+        }
+
+        // This is just a default constructor to initialize the default button fields
+        public RoundedButton() 
+        {
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 0;
+            this.Size = new Size(150, 40);
+            this.BackColor = Color.MediumSlateBlue;
+            this.ForeColor = Color.White;
+            this.Resize += new EventHandler(Button_Resize);
+        }
+
+        // What we do here is we created an eliptical figure using System.Drawing library.
+        private GraphicsPath GetFigurePath(Rectangle rect, int radius)
+        {
+            GraphicsPath path = new GraphicsPath();
+            float curveSize = radius * 2F;
+
+            path.StartFigure();
+
+            // AddArc functiion from System.Drawing appends an elliptical arc to the figure, which is passed to the function (rect in our case).
+            path.AddArc(rect.X, rect.Y, curveSize, curveSize, 180, 90);
+            path.AddArc(rect.Right - curveSize, rect.Y, curveSize, curveSize, 270, 90);
+            path.AddArc(rect.Right - curveSize, rect.Bottom - curveSize, curveSize, curveSize, 0, 90);
+            path.AddArc(rect.X, rect.Bottom - curveSize, curveSize, curveSize, 90, 90);
+
+            path.CloseFigure();
+
+            return path;
+        }
+
+        protected override void OnPaint(PaintEventArgs paint)
+        {
+            base.OnPaint(paint);
+
+            Rectangle rectSurface = this.ClientRectangle;
+            Rectangle rectBorder = Rectangle.Inflate(rectSurface, -borderSize, -borderSize);
+            int smoothSize = 2;
+            if (borderSize > 0)
+                smoothSize = borderSize;
+
+            if (borderRadius > 2) //Rounded button
+            {
+                using (GraphicsPath pathSurface = GetFigurePath(rectSurface, borderRadius))
+                using (GraphicsPath pathBorder = GetFigurePath(rectBorder, borderRadius - borderSize))
+                using (Pen penSurface = new Pen(this.Parent.BackColor, smoothSize))
+                using (Pen penBorder = new Pen(borderColor, borderSize))
+                {
+                    paint.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                    //Button surface
+                    this.Region = new Region(pathSurface);
+                    //Draw surface border for HD result
+                    paint.Graphics.DrawPath(penSurface, pathSurface);
+
+                    //Button border                    
+                    if (borderSize >= 1)
+                        //Draw control border
+                        paint.Graphics.DrawPath(penBorder, pathBorder);
+                }
+            }
+            else //Normal button
+            {
+                paint.Graphics.SmoothingMode = SmoothingMode.None;
+                //Button surface
+                this.Region = new Region(rectSurface);
+                //Button border
+                if (borderSize >= 1)
+                {
+                    using (Pen penBorder = new Pen(borderColor, borderSize))
+                    {
+                        penBorder.Alignment = PenAlignment.Inset;
+                        paint.Graphics.DrawRectangle(penBorder, 0, 0, this.Width - 1, this.Height - 1);
+                    }
+                }
+            }
+        }
+
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+            this.Parent.BackColorChanged += new EventHandler(Container_BackColorChanged);
+        }
+
+        private void Container_BackColorChanged(object sender, EventArgs e)
+        {
+            this.Invalidate();
+        }
+
+        private void Button_Resize(object sender, EventArgs e)
+        {
+            if (borderRadius > this.Height)
+                borderRadius = this.Height;
+        }
+    }
+}
+
